4e9936cc07f44ea6f8679836e492b6e66928b90b
Changed <changed@MacBook-Air-Changed.local>
2015-07-03 21:54:09 +0300

    release 1.0

--- classes/steam.class.php
+++ classes/steam.class.php
@@ -1,9 +1,13 @@
 <?php
-error_reporting(E_ALL ^ E_NOTICE ^ E_WARNING);
 require_once 'Unirest.php';
 require_once 'simple_html_dom.php';
 /**
 * Steam Trade PHP Class
+* Based on node.js version by Alex7Kom https://github.com/Alex7Kom/node-steam-tradeoffers
+*
+*
+*
+* https://github.com/halipso/php-steam-tradeoffers
 */
 class SteamTrade
 {
@@ -45,7 +49,7 @@ class SteamTrade
 		}
 
 		if($parse->find('#bodyContents_ex',0)->find('h2',0)->plaintext == 'Your Steam Web API Key') {
-			$key = explode(' ',$parse->find('#bodyContents_ex',0)->find('p',0))[1];
+			$key = explode(' ',$parse->find('#bodyContents_ex',0)->find('p',0)->plaintext)[1];
 			$this->apiKey = $key;
 			return;
 		}
@@ -126,25 +130,25 @@ class SteamTrade
 		return $n_items;
 	}
 
-	private function toSteamID($id) {
+	private function toAccountID($id) {
 	    if (preg_match('/^STEAM_/', $id)) {
 	        $split = explode(':', $id);
 	        return $split[2] * 2 + $split[1];
 	    } elseif (preg_match('/^765/', $id) && strlen($id) > 15) {
 	        return bcsub($id, '76561197960265728');
 	    } else {
-	        return $id; // We have no idea what this is, so just return it.
+	        return $id;
 	    }
 	}
 
-	private function toAccountID($id) {
+	private function toSteamID($id) {
 	    if (preg_match('/^STEAM_/', $id)) {
 	        $parts = explode(':', $id);
 	        return bcadd(bcadd(bcmul($parts[2], '2'), '76561197960265728'), $parts[1]);
 	    } elseif (is_numeric($id) && strlen($id) < 16) {
 	        return bcadd($id, '76561197960265728');
 	    } else {
-	        return $id; // We have no idea what this is, so just return it.
+	        return $id;
 	    }
 	}
 
@@ -171,28 +175,29 @@ class SteamTrade
 		return $this->_loadInventory(array(), $uri, array(
 		    'json' => TRUE,
 		    'headers' => array(
-		      'referer' => 'https://steamcommunity.com/tradeoffer/'.$offer.'/?partner='.$this->toSteamID($options['partnerSteamId'])
+		      'referer' => 'https://steamcommunity.com/tradeoffer/'.$offer.'/?partner='.$this->toAccountID($options['partnerSteamId'])
 		    ) , $options['contextId'], null));	
 	}
 
 	public function makeOffer($options) {
+
 		$tradeoffer = array(
 		    'newversion' => TRUE,
 		    'version' => 2,
 		    'me' => array('assets' => $options['itemsFromMe'], 'currency' => array(), 'ready' => FALSE ),
-		    'them' => array('assets' => $options['itemsFromThem'], 'currency': array(), 'ready' => FALSE )
+		    'them' => array('assets' => $options['itemsFromThem'], 'currency' => array(), 'ready' => FALSE )
 	  	);
 
 	  	$formFields = array(
 		    'serverid' => 1,
-		    'sessionid' => $this->sessionID,
+		    'sessionid' => $this->sessionId,
 		    'partner' => $options['partnerSteamId'] ? $options['partnerSteamId'] : $this->toSteamID($options['partnerAccountId']),
 		    'tradeoffermessage' => $options['message'] ? $options['message'] : '',
-		    'json_tradeoffer' => http_build_query($tradeoffer);
+		    'json_tradeoffer' => json_encode($tradeoffer)
 	  	);	
 
 	  	$query = array(
-		    'partner' => $options['partnerAccountId'] ? $options['partnerAccountId'] : $this->toAccountID($options['partnerSteamId']);
+		    'partner' => $options['partnerAccountId'] ? $options['partnerAccountId'] : $this->toAccountID($options['partnerSteamId'])
 		);
 
 	  	if($options['accessToken']) {
@@ -208,10 +213,183 @@ class SteamTrade
 	  		$referer = 'https://steamcommunity.com/tradeoffer/new/?'.http_build_query($query);
 	  	}
 
-	  	$headers = array('referer'=>$referer);
+	  	$headers = array('Cookie' => $this->webCookies,'Timeout'=> Unirest\Request::timeout(5));
+	  	$headers['referer'] = $referer;
 	  	$response = Unirest\Request::post('https://steamcommunity.com/tradeoffer/new/send', $headers, $formFields);
-	  	print_r($response);
+	  	
+	  	if($response->code != 200) {
+	  		die('Error making offer! Server response code: '.$response->code);
+	  	}
 
+	  	$body = $response->body;
+
+	  	if($body && $body->strError) {
+	  		die('Error making offer: '.$body->strError);
+	  	}
+
+	  	return $body;
+	}
+
+	public function getOffers($options) {
+		$offers = $this->doAPICall(
+			array(
+				'method' => 'GetTradeOffers/v1',
+				'params' => $options
+			)
+		);
+
+		$offers = json_decode(mb_convert_encoding($offers, 'UTF-8', 'UTF-8'),1);
+
+		if($offers['response']['trade_offers_received']) {
+			foreach ($offers['response']['trade_offers_received'] as $key => $value) {
+				$offers['response']['trade_offers_received'][$key]['steamid_other'] = $this->toSteamID($value['accountid_other']);
+			}
+		}
+
+		if($offers['response']['trade_offers_sent']) {
+			foreach ($offers['response']['trade_offers_sent'] as $key => $value) {
+				$offers['response']['trade_offers_sent'][$key]['steamid_other'] = $this->toSteamID($value['accountid_other']);
+			}
+		}
+
+		return $offers;
+	}
+
+	public function getOffer($options) {
+		$offer = $this->doAPICall(
+			array(
+				'method' => 'GetTradeOffer/v1',
+				'params' => $options
+			)
+		);
+
+		$offer = json_decode(mb_convert_encoding($offer, 'UTF-8', 'UTF-8'),1);
+
+		if ($offer['response']['offer']) {
+	    	$offer['response']['offer']['steamid_other'] = $this->toSteamId($offer['response']['offer']['accountid_other']);
+	    }
+
+		return $offer;
+	} 
+
+	private function doAPICall($options) {
+		$uri = 'https://api.steampowered.com/IEconService/'.$options['method'].'/?key='.$this->apiKey.($options['post'] ? '' : ('&'.http_build_query($options['params'])));
+
+		$body = null;
+		if($options['post']) {
+			$body = $options['params'];
+		}
+
+		$response = ($options['post'] ? Unirest\Request::post($uri, null, $body) : Unirest\Request::get($uri));
+		
+		if($response->code != 200) {
+			die('Error doing API call. Server response code: '.$response->code);
+		}
+
+		if(!$response->raw_body) {
+			die('Error doing API call. Invalid response.');
+		}
+
+		return $response->raw_body;
+	}
+
+	public function declineOffer($options) {
+		return $this->doAPICall(
+			array(
+				'method' => 'DeclineTradeOffer/v1',
+				'params' => array('tradeofferid' => $options['tradeOfferId']),
+				'post' => 1
+			)
+		);
+	}
+
+	public function cancelOffer($options) {
+		return $this->doAPICall(
+			array(
+				'method' => 'CancelTradeOffer/v1',
+				'params' => array('tradeofferid' => $options['tradeOfferId']),
+				'post' => 1
+			)
+		);
+	}
+
+	public function acceptOffer($options) {
+
+	  	if(!$options['tradeOfferId']) {
+	  		die('No options');
+	  	}
+
+	  	$form = array(
+	  		'sessionid' => $this->sessionId,
+	  		'serverid' => 1,
+	  		'tradeofferid' => $options['tradeOfferId']
+	  		);
+
+	  	$referer = 'https://steamcommunity.com/tradeoffer/'.$options['tradeOfferId'].'/';
+
+	  	$headers = array('Cookie' => $this->webCookies,'Timeout'=> Unirest\Request::timeout(5));
+	  	$headers['referer'] = $referer;
+	  	$response = Unirest\Request::post('https://steamcommunity.com/tradeoffer/'.$options['tradeOfferId'].'/accept', $headers, $form);
+
+	  	if($response->code != 200) {
+	  		die('Error accepting offer. Server response code: '.$response->code);
+	  	}
+
+	  	$body = $response->body;
+
+	  	if($body && $body->strError) {
+	  		die('Error accepting offer: '.$body->strError);
+	  	}
+
+	  	return $body;
+	}
+
+	public function getOfferToken() {
+
+		$headers = array('Cookie' => $this->webCookies,'Timeout'=> Unirest\Request::timeout(5));
+		$response = Unirest\Request::get('https://steamcommunity.com/my/tradeoffers/privacy', $headers);
+
+		if($response->code != 200) {
+			die('Error retrieving offer token. Server response code: '.$response->code);
+		}
+
+		$body = str_get_html($response->body);
+
+		if(!$body) {
+			die('Error retrieving offer token. Invalid response.');
+		}
+
+    	$offerUrl = $body->find('#trade_offer_access_url',0)->value;
+    	return explode('=',$offerUrl)[2];
+	}
+
+	public function getItems($options) {
+		$headers = array('Cookie' => $this->webCookies,'Timeout'=> Unirest\Request::timeout(5));
+		$response = Unirest\Request::get('https://steamcommunity.com/trade/'.$options['tradeId'].'/receipt/', $headers);
+
+		if($response->code != 200) {
+			die('Error get items. Server response code: '.$response->code);
+		}
+
+		$body = $response->body;
+
+		preg_match('/(var oItem;[\s\S]*)<\/script>/', $body, $matches);
+
+		if(!$matches) {
+			die('Error get items: no session');
+		}
+
+		$temp = str_replace(array("\r", "\n"), "", $matches[1]);
+
+		$items = array();
+
+		preg_match_all('/oItem = {(.*?)};/', $temp, $matches);
+		foreach ($matches[0] as $key => $value) {
+			$value = rtrim(str_replace('oItem = ', '', $value),';');
+			$items[] = json_decode($value,1);
+		}
+
+		return $items;
 	}
 
 }
--- index.php
+++ index.php
@@ -1,13 +1,9 @@
 <?php
-error_reporting(E_ALL);
 require_once('classes/steam.class.php');
+
 $steam = new SteamTrade();
 $steam->setup(
 	'dcbf1f030a7517b532dd2a20',
-	'__utma=268881843.1968640959.1431894014.1435827871.1435834368.187;__utmb=268881843.0.10.1435834368;__utmc=268881843;__utmz=268881843.1435764757.179.84.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=(not%20provided);mediumappid=753;mediumname=368020-Mysterious Card 3 (Foil);recentlyVisitedAppHubs=319630%2C261570%2C252490%2C730;sessionid=dcbf1f030a7517b532dd2a20;steamCountry=RU%7Cb74c0a38caa0109ecfed3984c3f00a4b;steamLogin=76561198043696486%7C%7C3253143FA887C5B862476A11AC02134E759F8AD9;steamLoginSecure=76561198043696486%7C%7C97F8F8DC7F20EB48D95ADCDC210025CBC92831B6;steamMachineAuth76561198043696486=6C090F7DF6FE5728FF2CF360EF83AE623D3C3767;steamMachineAuth76561198091831085=6F4D0B2EAED38A7B776EE85BFA3AA1DB38211C41;strInventoryLastContext=730_2;timezoneOffset=10800,0;webTradeEligibility=%7B%22allowed%22%3A1%2C%22allowed_at_time%22%3A0%2C%22steamguard_required_days%22%3A15%2C%22sales_this_year%22%3A296%2C%22max_sales_per_year%22%3A-1%2C%22forms_requested%22%3A0%2C%22new_device_cooldown_days%22%3A7%7D;'
+	'__utma=268881843.1968640959.1431894014.1435852298.1435855800.192;__utmb=268881843.0.10.1435855800;__utmc=268881843;__utmz=268881843.1435855800.192.87.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=(not%20provided);gsScrollPos=;mediumappid=753;mediumname=368020-Mysterious Card 3 (Foil);recentlyVisitedAppHubs=319630%2C261570%2C252490%2C730;sessionid=dcbf1f030a7517b532dd2a20;steamCountry=RU%7Cb74c0a38caa0109ecfed3984c3f00a4b;steamLogin=76561198043696486%7C%7C3253143FA887C5B862476A11AC02134E759F8AD9;steamLoginSecure=76561198043696486%7C%7C97F8F8DC7F20EB48D95ADCDC210025CBC92831B6;steamMachineAuth76561198043696486=6C090F7DF6FE5728FF2CF360EF83AE623D3C3767;steamMachineAuth76561198091831085=6F4D0B2EAED38A7B776EE85BFA3AA1DB38211C41;strInventoryLastContext=730_2;timezoneOffset=10800,0;tsTradeOffersLastRead=1435857240;webTradeEligibility=%7B%22allowed%22%3A1%2C%22allowed_at_time%22%3A0%2C%22steamguard_required_days%22%3A15%2C%22sales_this_year%22%3A296%2C%22max_sales_per_year%22%3A-1%2C%22forms_requested%22%3A0%2C%22new_device_cooldown_days%22%3A7%7D;'
 	);
-//$my = $steam->loadMyInventory(array('appId'=>730,'contextId'=>2));
-
-$partner = $steam->loadPartnerInventory(array('partnerSteamId'=>76561198053328708,'appId'=>730,'contextId'=>2));
-print_r($partner);
 ?>
